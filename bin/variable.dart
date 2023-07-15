07d9141a03139789780576328a9500c9f76188bd
lexynotfound <raihanardila@gmail.com>
2023-07-15 09:49:13 +0700

    penambahan variable

---
+++ bin/variable.dart
@@ -0,0 +1,28 @@
+void main(){
+  /* Tanpa Variable */
+  print('Kurnia Raihan Ardian');
+  print('Kurnia Raihan Ardian');
+  print('Kurnia Raihan Ardian');
+  print('Kurnia Raihan Ardian');
+
+  /* Menggunakan Variable */
+
+  String name;
+  name = 'Kurnia Raihan ARDIAN';
+
+  print(name);
+  print(name);
+  print(name);
+  print(name);
+
+  /* Menggunakan Deklarasi Variable Langsun */
+
+  String names = 'Kurnia RAIHAN ARDIAN';
+
+  print(names);
+  print(names);
+  print(names);
+  print(names);
+
+
+}
