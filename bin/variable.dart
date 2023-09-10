c55efb5de7e84fc2a61aabed08ed790179175d75
lexynotfound <raihanardila@gmail.com>
2023-09-11 03:56:58 +0700

    release updated

--- bin/variable.dart
+++ bin/variable.dart
@@ -24,5 +24,45 @@ void main(){
   print(names);
   print(names);
 
+  /* Menggunakan Deklarasi Variable CamelcaseLangsung */
+
+  String firstName = 'Kurnia RAIHAN ARDIAN - 2122010018';
+
+  print(firstName);
+  print(firstName);
+
+  /* Menggunakan Deklarasi Variable CamelcaseLangsung tipe data int*/
+
+  int aGe = 1;
+
+  print(aGe);
+
+  /* Menggunakan Deklarasi Variable CamelcaseLangsung tipe data boolean*/
+
+  bool aGes = true;
+
+  print(aGes);
+
+  /* Menggunakan Deklarasi Variable CamelcaseLangsung tipe data double*/
+
+  double dB = 9.8;
+
+  print(dB);
+
+  /* Menggunakan Deklarasi Variable CamelcaseLangsung var untuk mendifiniskan seluruh tipe data tanpa harus menuliskan int,string,boolean,dll.*/
+
+  var dbS = 9.8;
+
+  print(dbS);
+
+  /* Menggunakan Deklarasi Variable Camelcase Langsung  tidak bisa menggunakan int,boolean dll. karna harus sesuai dengan tipe datanya
+    Akan muncul error
+  */
+
+/*  String firstName = 'Kurnia RAIHAN ARDIAN - 2122010018';
+
+  print(firstName);
+  print(firstName); */
+
 
 }
